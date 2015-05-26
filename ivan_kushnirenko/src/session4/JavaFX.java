ac10fb1b3186003e23ea61549cd9c07ee4997f8f
ivan_kushnirenko <kushnirenkoivan@gmail.com>
2015-05-26 20:48:05 +0300

    module added by ivan_kushnirenko

--- ivan_kushnirenko/src/session4/JavaFX.java
+++ ivan_kushnirenko/src/session4/JavaFX.java
@@ -4,7 +4,11 @@ package session4;/**
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class JavaFX extends Application {
@@ -15,6 +19,11 @@ public class JavaFX extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(new TextField("text"));
+        borderPane.setCenter(new TextArea("qwerty"));
+        borderPane.setLeft(new Label("Some text is here"));
+        borderPane.setBottom(new Button("Ok"));
         Scene scene = new Scene(new Label("hi!"),600,400);
         primaryStage.setScene(scene);
         primaryStage.show();
