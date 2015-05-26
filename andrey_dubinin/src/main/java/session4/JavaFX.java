708e0c367992f42efad9ecf6fbe8ee4a28f37972
Andrey_Dubinin <AndreyD100@gmail.com>
2015-05-26 20:53:26 +0300

    test commit

--- andrey_dubinin/src/main/java/session4/JavaFX.java
+++ andrey_dubinin/src/main/java/session4/JavaFX.java
@@ -2,7 +2,13 @@ package session4;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -12,7 +18,13 @@ public class JavaFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(new Label("hi"), 600,400);
+        BorderPane pane = new BorderPane();
+        pane.setTop(new TextField("text"));
+        pane.setCenter(new TextArea("1\n2"));
+        pane.setLeft(new Label("Просто текст"));
+        pane.setBottom(new Button("OK"));
+
+        Scene scene = new Scene(pane,600,400);
         stage.setTitle("Hello FX");
         stage.setScene(scene);
         stage.show();
