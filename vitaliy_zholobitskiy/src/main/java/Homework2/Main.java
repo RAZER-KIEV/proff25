ef28289780d7c883bd8705beba991a05993aa0ab
just1ce2194 <just1ce2194@gmail.com>
2015-05-28 21:08:14 +0300

    Done fourth task from second Homework!

---
+++ vitaliy_zholobitskiy/src/main/java/Homework2/Main.java
@@ -0,0 +1,14 @@
+package Homework2;
+
+import java.io.IOException;
+
+/**
+ * Created by just1ce on 28.05.2015.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        TestFreq t= new TestFreq();
+        t.test();
+    }
+}
