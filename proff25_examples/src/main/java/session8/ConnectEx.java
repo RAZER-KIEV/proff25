68fe42f16bee144f8cd91efe815669149bf032bf
alex-zr <oleksandr.roshchupkin@gmail.com>
2015-05-29 21:37:13 +0300

    jdbc example

--- proff25_examples/src/main/java/session8/ConnectEx.java
+++ proff25_examples/src/main/java/session8/ConnectEx.java
@@ -1,10 +1,5 @@
 package session8;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Locale;
-
 /**
  * Created with IntelliJ IDEA.
  * User: al1
@@ -12,31 +7,6 @@ import java.util.Locale;
  */
 public class ConnectEx {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.ENGLISH);
-        System.out.println("Application started...");
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver not found");
-            e.printStackTrace();
-            return;
-        }
-        System.out.println("JDBC driver is loaded!");
 
-        Connection conn = null;
-        String url = "jdbc:oracle:thin:@localhost:1521:XE";
-        try {
-            conn = DriverManager.getConnection(url, "hr", "hr");
-        } catch (SQLException e) {
-            System.out.println("Connection failed");
-            e.printStackTrace();
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
     }
 }
