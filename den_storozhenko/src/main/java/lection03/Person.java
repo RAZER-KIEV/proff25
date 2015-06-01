e3208d946696d72a3981339d48d67a333460096f
Den <storozenko@inbox.ru>
2015-06-01 21:28:04 +0300

    Lection2

---
+++ den_storozhenko/src/main/java/lection03/Person.java
@@ -0,0 +1,39 @@
+package lection03;
+
+/**
+ * Created by Taras on 01.06.2015.
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public  Person(){
+        name = "";
+        age = 0;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+}
