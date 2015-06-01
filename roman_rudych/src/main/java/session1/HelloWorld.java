c456af5334ea48fe67e396f9754ed4a4aeae0ed1
roman_rudych <roman@a.com>
2015-06-01 16:44:22 +0300

    Commit after finishing the HW

--- roman_rudych/src/main/java/session1/HelloWorld.java
+++ roman_rudych/src/main/java/session1/HelloWorld.java
@@ -1,12 +1,11 @@
 package session1;
 
+
 /**
- * Created with IntelliJ IDEA.
- * User: al1
- * Date: 22.04.15
+ * Created by Роман on 18.05.2015.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Intellij");
+        System.out.println("Hello world");
     }
 }
