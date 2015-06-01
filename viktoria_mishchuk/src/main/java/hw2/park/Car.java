47334a2273d3f13210689d5aefc5d8595435303d
viktoria <viktoriia.mishchuk@gmail.com>
2015-06-01 14:09:35 +0300

    hw2.park

---
+++ viktoria_mishchuk/src/main/java/hw2/park/Car.java
@@ -0,0 +1,19 @@
+package hw2.park;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by viktoria on 01.06.15.
+ */
+public class Car {
+    private Integer placeNumber;
+
+    public void setPlaceNumber(Integer placeNumber) {
+        this.placeNumber = placeNumber;
+    }
+
+    public int getPlaceNumber() {
+        return placeNumber;
+    }
+}
---
+++ viktoria_mishchuk/src/main/java/hw2/park/ParkFullException.java
@@ -0,0 +1,11 @@
+package hw2.park;
+
+/**
+ * Created by viktoria on 01.06.15.
+ */
+public class ParkFullException extends Exception {
+
+    public ParkFullException(String msg){
+        super(msg);
+    }
+}
