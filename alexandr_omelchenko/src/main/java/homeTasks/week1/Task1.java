c8fbd353cf98192d7ba9323db2672414bec097ab
HP <ooomelchenko@gmail.com>
2015-06-01 18:02:15 +0300

    alexandr_omelchenko add packages from prof25 to proff25

--- alexandr_omelchenko/src/main/java/homeTasks/week1/Task1.java
+++ alexandr_omelchenko/src/main/java/homeTasks/week1/Task1.java
@@ -1,8 +1,6 @@
-package HT1;
+package homeTasks.week1;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
 
 public class Task1 {
     public static void main(String[] args) {
---
+++ alexandr_omelchenko/src/main/java/homeTasks/week2/User.java
@@ -0,0 +1,7 @@
+package homeTasks.week2;
+
+/**
+ * Created by HP on 31.05.2015.
+ */
+public class User {
+}
