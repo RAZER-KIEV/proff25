f9b16be11724e7bee5aeeb0f31061c004850ea44
Sveta <tutochechka@gmail.com>
2015-06-03 15:01:37 +0300

    session 6 and HW 2.4

--- svetlana_gazina/src/main/java/Homework2/frequency/Freq.java
+++ svetlana_gazina/src/main/java/Homework2/frequency/Freq.java
@@ -1,8 +1,6 @@
 package Homework2.frequency;
 
-import scala.collection.Set;
-
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Sveta on 5/30/2015.
@@ -20,13 +18,31 @@ import java.util.Scanner;
  */
 public class Freq {
     String text;
+    HashMap<String, Integer> frequencies = new HashMap<String, Integer>();
+
+    public HashMap getFrequencies(String text){
+        String[] words = text.split(" ");
+        for(String word: words){
+            if(frequencies.containsKey(word)){
+                int value = frequencies.get(word) + 1;
+                frequencies.put(word, value);
+            }
+            else {
+                frequencies.put(word, 1);
+            }
+        }
+        return frequencies;
+    }
 
  public String setTextFromConsole(){
   Scanner scanner = new Scanner(System.in);
      text = "";
      while(scanner.hasNext())
      {
-         text += scanner.nextLine();
+         if(scanner.next() != "end!") {
+             text += scanner.nextLine();
+         }
+         break;
      }
      return  text;
  }
@@ -42,36 +58,100 @@ public class Freq {
   return text;
  }
 
- public String generateRandomText(int textLength){
+ public String generateRandomText(){
+     char[] chars = "abc def ghi jkl mno pqr stu vwx yz ".toCharArray();
+     StringBuilder sb = new StringBuilder();
+     Random random = new Random();
+     for (int i = 0; i < 2000; i++) {
+         char aChar = chars[random.nextInt(chars.length)];
+         sb.append(aChar);
+     }
+     text = sb.toString();
+
   return text;
  }
 
  public Set<String> getWordsByFrequency(int frequency){
-  return null;
+     Set<String> result = null;
+     Map words = getFrequencies(text);
+     Set<String> keys = words.keySet();
+     for(String word: keys){
+         if((int)words.get(word) == frequency){
+             result.add(word);
+         }
+     }
+
+     return result;
  }
  public Set<String> getWordsByFrequencyLessThan(int frequency){
-  return null;
+     Set<String> result = null;
+
+     Map words = getFrequencies(text);
+     Set<String> keys = words.keySet();
+     for(String word: keys){
+         if((int)words.get(word) < frequency){
+             result.add(word);
+         }
+     }
+
+     return result;
  }
  public Set<String> getWordsByFrequencyMoreThan(int frequency){
-  return null;
+     Set<String> result = null;
+
+     Map words = getFrequencies(text);
+     Set<String> keys = words.keySet();
+     for(String word: keys){
+         if((int)words.get(word) > frequency){
+             result.add(word);
+         }
+     }
+
+     return result;
  }
- public void printAcs(){
+    public void printAcs(){
+        Map<String, Integer> words = getFrequencies(text);
 
- }
+        Map<String, Integer> mapSort = new TreeMap<String, Integer>(new Comparator<String>() {
+            @Override
+            public int compare(String str1, String str2) {
+                return words.get(str1).compareTo(words.get(str2));
+            }
+        });
+        mapSort.putAll(words);
+        for(Map.Entry<String, Integer> entry : mapSort.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 
- public String getText() {
-  return text;
- }
+    }
 
-public void printDesc() {
+    public void printDesc(){
+        Map<String, Integer> words = getFrequencies(text);
 
-}
+        Map<String, Integer> mapSort = new TreeMap<String, Integer>(new Comparator<String>() {
+            @Override
+            public int compare(String str1, String str2) {
+                return words.get(str2).compareTo(words.get(str1));
+            }
+        });
+        mapSort.putAll(words);
+        for(Map.Entry<String, Integer> entry : mapSort.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+    }
+
 
 }
 
 class FreqTest {
- public static void main(String[] args) {
-  Freq freq = new Freq();
-  System.out.println(freq.setTextFromConsole());
- }
+    public static void main(String[] args) {
+        Freq freq = new Freq();
+        String text = freq.setTextFromConsole();
+        Map<String,Integer> map = freq.getFrequencies(text);
+        System.out.println(map);
+
+        freq.printAcs();
+        freq.printDesc();
+    }
 }
