dd7eae0036c96adb8dc4fc839ef5a31841898e26
nick_martyn <w_nick@ukr.net>
2015-06-06 22:00:22 +0300

    first commit

---
+++ nick_martyn/src/main/session1/HelloWorld.java
@@ -0,0 +1,16 @@
+package session1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: al1
+ * Date: 22.04.15
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Intellij");
+        //
+        //
+        //
+        //
+    }
+}
