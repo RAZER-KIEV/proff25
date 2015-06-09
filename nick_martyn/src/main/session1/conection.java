2d8c747fa568d4b2cae5bf251eb9c10610462ad2
nick_martyn <w_nick@ukr.net>
2015-06-09 21:50:41 +0300

    first commit

--- nick_martyn/src/main/session1/conection.java
+++ nick_martyn/src/main/session1/conection.java
@@ -7,10 +7,10 @@ package session1;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Intellij");
-        //
-        //
-        //
-        //
+
+
+
+
+
     }
 }
