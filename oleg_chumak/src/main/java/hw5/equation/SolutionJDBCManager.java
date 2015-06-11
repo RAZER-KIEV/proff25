cf852845f0fabdee02ee3b0303b3d41c5a624c50
Oleg <oleg4ymak@gmail.com>
2015-06-11 22:05:47 +0300

    hw5.equation

--- oleg_chumak/src/main/java/hw5/equation/SolutionJDBCManager.java
+++ oleg_chumak/src/main/java/hw5/equation/SolutionJDBCManager.java
@@ -10,8 +10,9 @@ import java.util.Locale;
 public class SolutionJDBCManager {
     private Statement stmnt;
     private Connection conn;
+    private Path path;
     QuadraticService qss = new QuadraticService();
-
+    public Solution solution;
     public SolutionJDBCManager() throws SQLException {
         Locale.setDefault(Locale.ENGLISH);
         conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hr", "hr");
@@ -37,6 +38,27 @@ public class SolutionJDBCManager {
             return 0;
         }
     }
+
+    public int create(Path path) throws SQLException {
+        try {
+            PreparedStatement pstmnt = conn.prepareStatement("INSERT INTO EQUATION(ID, KOEF_A, KOEF_B, KOEF_C, RES_1, RES_2) VALUES (?, ?, ?, ?, ?, ?)");
+            pstmnt.setInt(1, solution.getId());
+            pstmnt.setInt(2, solution.getKoef_a());
+            pstmnt.setInt(3, solution.getKoef_b());
+            pstmnt.setInt(4, solution.getKoef_c());
+            pstmnt.setInt(5, solution.getRes_1());
+            if(solution.getRes_2()!=null) {
+                pstmnt.setInt(6, solution.getRes_2());
+            }    else   {
+                pstmnt.setInt(6, ' ');
+            }
+            pstmnt.executeUpdate();
+            return 1;
+        } catch (SQLException except){
+            return 0;
+        }
+    }
+
 
     public List<Solution> findAll(){
         return qss.list;
