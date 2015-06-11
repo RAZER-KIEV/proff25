bca1d724a185760c294fd1c6b84472bade568395
denst <denstden@gmail.com>
2015-06-11 15:41:57 +0300

    hw4.chat

--- den_storozhenko/src/main/java/hw3/chat/AsyncChat.java
+++ den_storozhenko/src/main/java/hw3/chat/AsyncChat.java
@@ -6,6 +6,10 @@ import lection03.NewServer;
  * Created by storo_000 on 11.06.2015.
  */
 public class AsyncChat {
+    public void process(){
+        new Thread(new NewServer()).start();
+    }
+
     public static void main(String[] args) {
         new Thread(new NewServer()).start();
     }
