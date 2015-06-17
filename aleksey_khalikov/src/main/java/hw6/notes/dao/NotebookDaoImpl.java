7ed75148219f41415a68488a76a1efca06688d92
aleksey_khalikov <aleksey_h@ukr.net>
2015-06-17 22:57:05 +0300

    hw6 part 1

---
+++ aleksey_khalikov/src/main/java/hw6/notes/dao/NotebookDaoImpl.java
@@ -0,0 +1,96 @@
+package hw6.notes.dao;
+
+import hw6.notes.domain.Notebook;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+/**
+ * Created by GFalcon on 17.06.15.
+ */
+public class NotebookDaoImpl implements NotebookDao {
+    private static Logger log = Logger.getLogger(NotebookDaoImpl.class);
+    private SessionFactory factory;
+
+    public NotebookDaoImpl(SessionFactory factory){
+        this.factory = factory;
+    }
+
+    @Override
+    public Long create(Notebook ntb) {
+        Long id = null;
+        Session session = factory.openSession();
+        try {
+            session.getTransaction().begin();
+            id = (Long)session.save(ntb);
+            session.getTransaction().commit();
+        } catch (HibernateException e){
+            log.error("Create transaction error: " + e.getMessage());
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return id;
+    }
+
+    @Override
+    public Notebook read(Long ig) {
+        Session session = factory.openSession();
+        try {
+            return (Notebook)session.get(Notebook.class, ig);
+        } catch (HibernateException e){
+            log.error("Read operation error: " + e.getMessage());
+        } finally {
+            session.close();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean update(Notebook ntb) {
+        boolean res = false;
+        Session session = factory.openSession();
+        try {
+            session.getTransaction().begin();
+            session.update(ntb);
+            session.getTransaction().commit();
+            res = true;
+        } catch (HibernateException e){
+            log.error("Update error: " + e.getMessage());
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return res;
+    }
+
+    @Override
+    public boolean delete(Notebook ntb) {
+        boolean res = false;
+        Session session = factory.openSession();
+        try {
+            session.getTransaction().begin();
+            session.delete(ntb);
+            session.getTransaction().commit();
+            res = true;
+        } catch (HibernateException e){
+            log.error("Delete operation error: " + e.getMessage());
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return res;
+    }
+
+    @Override
+    public List<Notebook> findAll() {
+        Session session = factory.openSession();
+        Query query = session.createQuery("from Notebook");
+        session.close();
+        return query.list();
+    }
+}
---
+++ aleksey_khalikov/src/main/java/hw6/notes/domain/Notebook.java
@@ -0,0 +1,88 @@
+package hw6.notes.domain;
+
+import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by GFalcon on 17.06.15.
+ */
+@Entity
+@Table(name="NOTEBOOK")
+public class Notebook {
+    @SequenceGenerator(name = "myseq", sequenceName = "SEQ_NOTEBOOK_ID", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "myseq")
+
+    @Id
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "SERIAL")
+    private String serial;
+    @Column(name = "VENDOR")
+    private String vendor;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "MANUFACTURE_DATE")
+    private Date manufactureDate;
+    @Column(name = "PRICE")
+    private Double price;
+
+    public Notebook(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String mdate = dateFormat.format(getManufactureDate());
+        return getVendor() + " " + getModel() + " SN: " + getSerial() + " manuf. " + mdate + "\t price: " + getPrice();
+    }
+}
