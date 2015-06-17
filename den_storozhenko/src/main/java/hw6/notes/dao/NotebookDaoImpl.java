24fe40aef52febbeb032e0fb78dcc68b01c8ab01
denst <denstden@gmail.com>
2015-06-17 14:15:06 +0300

    HibernateDAO

---
+++ den_storozhenko/src/main/java/hw6/notes/dao/NotebookDaoImpl.java
@@ -0,0 +1,93 @@
+package hw6.notes.dao;
+
+import hw6.notes.domain.Notebook;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import java.util.List;
+
+
+public class NotebookDaoImpl implements NotebookDao {
+    private static Logger log = Logger.getLogger(NotebookDaoImpl.class);
+    private SessionFactory factory;
+
+    public NotebookDaoImpl(SessionFactory factory){
+        this.factory = factory;
+    }
+
+    @Override
+    public Long create(Notebook ntb) {
+        Session session = factory.openSession();
+        Long id = null;
+        try {
+            session.beginTransaction();
+            id = (Long)session.save(ntb);
+            session.getTransaction().commit();
+            return id;
+        }catch (HibernateException e){
+            log.error("Transaction failed");
+            session.getTransaction().rollback();
+        }finally {
+            if (session!=null)
+                session.close();
+        }
+        return id;
+    }
+
+    @Override
+    public Notebook read(Long id) {
+        Session session = factory.openSession();
+        try{
+            return (Notebook)session.get(Notebook.class,id);
+        }catch (HibernateException e){
+            log.error("Transaction failed");
+        }finally {
+            if (session!=null)
+                session.close();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean update(Notebook ntb) {
+        Session session = factory.openSession();
+        try {
+            session.beginTransaction();
+            session.update(ntb);
+            session.getTransaction().commit();
+            return true;
+        }catch (HibernateException e){
+            log.error("Transaction failed");
+            session.getTransaction().rollback();
+            return false;
+        }finally {
+            if (session!=null)
+                session.close();
+        }
+    }
+
+    @Override
+    public boolean delete(Notebook ntb) {
+        Session session = factory.openSession();
+        try {
+            session.beginTransaction();
+            session.delete(ntb);
+            session.getTransaction().commit();
+            return true;
+        }catch (HibernateException e){
+            log.error("Transaction failed");
+            session.getTransaction().rollback();
+            return false;
+        }finally {
+            if (session!=null)
+                session.close();
+        }
+    }
+
+    @Override
+    public List findAll() {
+        Session session = factory.openSession();
+        return session.createQuery("from Notebook").list();
+    }
+}
