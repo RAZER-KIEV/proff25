8d0cf4a1e20312c10530f50ea890320011a8dd78
aleksey_khalikov <aleksey_h@ukr.net>
2015-06-18 00:15:43 +0300

    NotebookDaoImpl with default constructor

--- aleksey_khalikov/src/main/java/hw6/notes/dao/NotebookDaoImpl.java
+++ aleksey_khalikov/src/main/java/hw6/notes/dao/NotebookDaoImpl.java
@@ -16,6 +16,10 @@ public class NotebookDaoImpl implements NotebookDao {
     private static Logger log = Logger.getLogger(NotebookDaoImpl.class);
     private SessionFactory factory;
 
+    public NotebookDaoImpl(){
+
+    }
+
     public NotebookDaoImpl(SessionFactory factory){
         this.factory = factory;
     }
