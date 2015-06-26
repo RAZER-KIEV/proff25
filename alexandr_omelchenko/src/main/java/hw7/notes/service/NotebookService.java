4e260f1104d6add43d271d308ce5f1ff9f4e5833
HP <ooomelchenko@gmail.com>
2015-06-26 18:38:43 +0300

    hw7 updete Service

--- alexandr_omelchenko/src/main/java/hw7/notes/service/NotebookService.java
+++ alexandr_omelchenko/src/main/java/hw7/notes/service/NotebookService.java
@@ -1,7 +1,17 @@
 package hw7.notes.service;
 
-/**
- * Created by HP on 26.06.2015.
- */
+import hw7.notes.domain.*;
+
 public interface NotebookService {
+    Long create(CPU processor);
+    Long create(Memory memory);
+    Long create(Vendor vendor);
+    Long create(Notebook notebook);
+    Long receive(Long id, int amount, double price);
+    Long sale(Long storeId, int amount);
+    boolean updateCPU(CPU cpu);
+    boolean updateMemory(Memory memory);
+    boolean updateVendor(Vendor vendor);
+    boolean updateNotebook(Notebook notebook);
+    boolean removeFromStore(Store store, int amount);
 }
