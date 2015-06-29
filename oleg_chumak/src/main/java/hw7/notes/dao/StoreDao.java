d713e0a6827391997abd54707ba1aeae97ca5fdc
Oleg <oleg4ymak@gmail.com>
2015-06-29 18:01:16 +0300

    hw7.Note

--- oleg_chumak/src/main/java/hw7/notes/dao/StoreDao.java
+++ oleg_chumak/src/main/java/hw7/notes/dao/StoreDao.java
@@ -1,8 +1,11 @@
 package hw7.notes.dao;
 
+import hw7.notes.domain.Notebook;
 import hw7.notes.domain.Store;
+import hw7.notes.domain.Vendor;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by oleg on 24.06.15.
@@ -13,4 +16,9 @@ public interface StoreDao {
     boolean update(Store store);
     boolean delete(Store store);
     List findAll();
+    List<Notebook> getNotesPorced(int start, int size);
+    List getNotebooksFromStore();
+    List getNotebooksGtAmount(int amount);
+    List getNotebooksByCpuVendor(Vendor cpuVendor);
+    Map getNotebooksStorePresent();
 }
