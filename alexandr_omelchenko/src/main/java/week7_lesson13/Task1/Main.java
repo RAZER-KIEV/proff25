47e8b0f5b117451e5a5b4932ce9f920c497e82a3
HP <ooomelchenko@gmail.com>
2015-06-30 14:12:00 +0300

    update session 13

--- alexandr_omelchenko/src/main/java/week7_lesson13/Task1/Main.java
+++ alexandr_omelchenko/src/main/java/week7_lesson13/Task1/Main.java
@@ -10,14 +10,13 @@ import week7_lesson13.Person;
 public class Main {
     public static void main(String[] args) {
 
-
     ApplicationContext context = new ClassPathXmlApplicationContext("session13/contextDirector.xml");
     Company comp = context.getBean("firm", Company.class);
- //   Director dir = context.getBean("dir", Director.class);
- //   Car car = context.getBean("car", Car.class);
+    Director dir = context.getBean("dir", Director.class);
+    Car car = context.getBean("car", Car.class);
 
     System.out.println(comp);
-  //  System.out.println(dir);
-  //  System.out.println(car);
+    System.out.println(dir);
+    System.out.println(car);
 }
 }
