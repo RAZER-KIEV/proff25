a02bdea487ce8e02340396cdff011c5962137d70
denst <denstden@gmail.com>
2015-07-03 14:53:25 +0300

    hw7.springnotes

--- den_storozhenko/src/main/java/hw7/springnotes/service/NotebookServiceImpl.java
+++ den_storozhenko/src/main/java/hw7/springnotes/service/NotebookServiceImpl.java
@@ -4,6 +4,7 @@ import hw7.springnotes.dao.*;
 import hw7.springnotes.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Calendar;
@@ -11,7 +12,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-@Component
+@Service
 @Transactional
 public class NotebookServiceImpl implements NotebookService {
     @Autowired
