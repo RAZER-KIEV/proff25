f7039dd7b22193a398102d46db7cec943a311d6c
HP <ooomelchenko@gmail.com>
2015-07-12 16:52:04 +0300

    session 15-16  and hw8 start

---
+++ alexandr_omelchenko_web/src/main/java/hw8/taxi/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package hw8.taxi.exception;
+
+/**
+ * Created by HP on 12.07.2015.
+ */
+public class AuthenticationException {
+}
--- alexandr_omelchenko_web/src/main/java/web/controller/HelloController.java
+++ alexandr_omelchenko_web/src/main/java/web/controller/HelloController.java
@@ -4,7 +4,6 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpSession;
 
 /**
@@ -18,30 +17,22 @@ public class HelloController {
     public static final Logger log = Logger.getLogger(HelloController.class);
 
     @RequestMapping(value = "/hello.html", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    String hello(Model model) {
-        log.info("/hello.html controller");
-        model.addAttribute("name", "Petro");
-        return "hello";
+    //public @ResponseBody String hello(Model model) {
+    public String hello(Model model) {
+       log.info("/hello.html controller");
+       model.addAttribute("name", "Admin");
+       return "index1";
     }
 
     @RequestMapping(value = "/great.html", method = RequestMethod.GET)
-    public String great(@RequestParam("login") String name, Model model, HttpSession session) {
+    public String great(@RequestParam("login") String name, Model model) {
         log.info("/great.html controller");
-        Long sessId = (Long) session.getAttribute("id");
-        if (sessId == null) {
-            return "index";
-        }
-
-        return "index";
+        model.addAttribute("hello", "hey, "+name);
+        return "index1";
     }
 
     @RequestMapping(value = "/form.html", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    String form(@RequestParam String login,
-                @RequestParam String pass) {
+    public @ResponseBody String form(@RequestParam String login, @RequestParam String pass) {
         return login + "[" + pass + "]";
     }
 
@@ -49,6 +40,6 @@ public class HelloController {
     public String index(Model model) {
         log.info("/index controller");
         model.addAttribute("name", "al1");
-        return "index";
+        return "index1";
     }
 }
