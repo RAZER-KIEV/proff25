a2a30d4b137c0215ffee8ad37efb182483454357
bosyi <bosyi@bosyi>
2015-07-14 22:12:37 +0300

    session11-12 classwork

--- bogdan_iavorskyi_web/src/main/java/scrum/controller/Controller.java
+++ bogdan_iavorskyi_web/src/main/java/scrum/controller/Controller.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import scrum.domain.Taxi;
 import scrum.domain.User;
 import scrum.service.TaxiService;
+
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -33,9 +36,11 @@ public class Controller {
     public String medium(@RequestParam("login") String login, String password, Model model) {
         log.info("/request.html controller");
 
-//        if (authenticate(login,password, service.getUserList())) {
-        if (0==0){
-            model.addAttribute("taxists",service.getTaxiList());
+        if (authenticate(login,password, service.getUserList())) {
+
+            List<Taxi> taxists = service.getTaxiList();
+            model.addAttribute("taxists", taxists);
+            System.out.println(taxists.size());
             return "dashboard";
         }
         else {
@@ -45,6 +50,7 @@ public class Controller {
     }
 
     private static boolean authenticate(String login, String password, List<User> users){
+        System.out.println(users.size());
         for (User user : users) {
             if (user.getLogin().equals(login)) {
                 if (user.getPass().equals(password)) {
