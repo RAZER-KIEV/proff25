b73228e11dbe0e572e7b9c16ac57c921ddb9ff3a
Well <wolverine13@ukr.net>
2015-07-21 21:54:43 +0300

    ver_2_a

---
+++ george_rublev_web/src/main/webapp/WEB-INF/jsps/indexTaxi.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Well
+  Date: 21.07.2015
+  Time: 21:07
+  To change this template use File | Settings | File Templates.
+--%>
+
+<html>
+<head>
+  <title>Taxi Last Way</title>
+  <script src="js/taxi.js"></script>
+
+</head>
+<body>
+
+<p>Login:  <input id="login" maxlength="25" size="40"></p>
+<p>Password: <input id="pass" maxlength="25" size="40"></p>
+
+<button onclick="check()">Submit</button>
+
+<div id="resp"> </div>
+
+</body>
+</html>
---
+++ george_rublev_web/src/main/webapp/js/taxi.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Well on 21.07.2015.
+ */
+
+var ajax = new XMLHttpRequest();
+var login = document.getElementById("login");
+var pass = document.getElementById("pass");
+ajax.onreadystatechange = function () {
+    if (ajax.readyState === 4 && ajax.status === 200) {
+        if (ajax.responseText !== 'false') {
+        var div = document.getElementById('div');
+            div = ajax.responseText.split('|');
+
+
+        } else {
+            alert('Login or Password - FAILED!!!!');
+        }
+    }
+    ;
+
+    function check() {
+        ajax.open('GET', '/ajax?login=' + login.value + '&pass=' + pass.value);
+        ajax.send();
+    }
+
+}
