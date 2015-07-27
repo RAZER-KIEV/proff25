442b8dd9ac853f5e743f213acb62cf280c0ccd4e
ivan_kushnirenko <kushnirenkoivan@gmail.com>
2015-07-27 14:58:53 +0300

    New changelist

---
+++ ivan_kushnirenko/src/main/java/hw5/finder/FileService.java
@@ -0,0 +1,61 @@
+package hw5.finder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by ivan on 26.07.15.
+ */
+public class FileService {
+
+    private ArrayList<Path> pathes;
+
+    public ArrayList<Path> getPathes() {
+        return pathes;
+    }
+
+    public void setPathes(ArrayList<Path> pathes) {
+        this.pathes = pathes;
+    }
+
+    public FileService() {
+        pathes = new ArrayList<Path>();
+        System.out.println("INFO: New FileServiceManager was created.");
+    }
+
+    public List searchFile(String fileName, String dirName) throws FileNotFoundException {
+        File searchDirectory = new File(dirName);
+        if (!searchDirectory.isDirectory()) {
+            throw new FileNotFoundException("'" + dirName + "' is not a directory.");
+        }
+        if (fileName.length() == 0) {
+            throw new IllegalArgumentException("File name must be non-empty.");
+        }
+        search(fileName, searchDirectory);
+        if (pathes.size() == 0) {
+            System.out.println("There are no such files in directory " + dirName + ".");
+        }
+        return pathes;
+    }
+
+    private void search(String fileName, File searchDirectory) {
+        if (searchDirectory.canRead()) {
+            for (File tmp : searchDirectory.listFiles()) {
+                if (tmp.isDirectory()) {
+                    search(fileName, tmp);
+                }
+                if ((tmp.getName().toString()).equalsIgnoreCase(fileName)) {
+                    pathes.add(new Path(tmp.getAbsolutePath(), new Date()));
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ ivan_kushnirenko/src/main/java/hw5/finder/MainWindow.java
@@ -0,0 +1,67 @@
+package hw5.finder;
+
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by ivan on 26.07.15.
+ */
+public class MainWindow {
+
+    private PathJDBCManager pathJDBCManager;
+    private FileService fileService;
+
+    public MainWindow() {
+        pathJDBCManager = new PathJDBCManager();
+        fileService = new FileService();
+    }
+
+    public PathJDBCManager getPathJDBCManager() {
+        return pathJDBCManager;
+    }
+
+    public void setPathJDBCManager(PathJDBCManager pathJDBCManager) {
+        this.pathJDBCManager = pathJDBCManager;
+    }
+
+    public FileService getFileService() {
+        return fileService;
+    }
+
+    public void setFileService(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    public String getData(String inputInfo) {
+        System.out.println(inputInfo);
+        String res = null;
+        Scanner scn = new Scanner(System.in);
+        while (scn.hasNext()) {
+            res = scn.next();
+        }
+        return res;
+    }
+
+    public void createDataBase() {
+        try {
+            List<Path> pathes = fileService.searchFile(getData("Type file name, please."), getData("Type path to search directory, please"));
+            if (pathes.size() != 0) {
+                for (Path path : pathes) {
+                    pathJDBCManager.create(path);
+                }
+            }
+        } catch (FileNotFoundException exp) {
+            exp.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MainWindow main = new MainWindow();
+
+    }
+}
+
+
