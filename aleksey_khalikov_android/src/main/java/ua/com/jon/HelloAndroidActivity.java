2c2372dfcd5cbfec5a909123eb9ab28ff5813260
aleksey_khalikov <aleksey_h@ukr.net>
2015-07-28 21:49:03 +0300

    android

---
+++ aleksey_khalikov_android/src/main/java/ua/com/jon/HelloAndroidActivity.java
@@ -0,0 +1,53 @@
+package ua.com.jon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class HelloAndroidActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        final Button hello = (Button) findViewById(R.id.button1);
+        final Button smallButton = (Button) findViewById(R.id.button);
+        final EditText txtField = (EditText) findViewById(R.id.editText2);
+        ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar);
+        Handler handler = new Handler();
+        // EditText text = (EditText)findViewById(R.id.editText);
+        hello.setOnClickListener(new OnClickListener() {
+            int i;
+
+            public void onClick(View v) {
+                hello.setText("Hello");
+                //      text.setText("Button");
+                CharSequence newText = (CharSequence) hello.getText();
+                txtField.setText(newText);
+
+            }
+        });
+        smallButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                txtField.setText(smallButton.getText());
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
+
