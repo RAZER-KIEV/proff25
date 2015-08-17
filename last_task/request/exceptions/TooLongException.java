2cfb3b7ac135e75882f105d4e2722cf7d4fdb1e4
viktoria <viktoriia.mishchuk@gmail.com>
2015-08-17 20:34:07 +0300

    exception2

---
+++ last_task/request/exceptions/TooLongException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+/**
+ * Created by viktoria
+ * Project:.exceptions
+ */
+public class TooLongException extends Exception {
+    public  TooLongException (String msg){
+        super(msg);
+    }
+}
