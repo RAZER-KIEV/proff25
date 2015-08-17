874b15c41fa5f8ef6d20c2461566f00b35683746
sviatoslav_ruzhickiy <sdlan-razer@ukr.net>
2015-08-17 21:28:57 +0300

    update classwork

--- roman_rudych/src/main/java/service/FileFindService.java
+++ roman_rudych/src/main/java/service/FileFindService.java
@@ -1,7 +1,60 @@
 package service;
 
+
+
+
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 /**
  * Created by Роман on 17.08.2015.
  */
-public class FileFindService {
+public class FileFindService implements FileFindServiceInteface {
+
+    class FileService {
+        String pathF;
+        FileSystem fileSystem;
+        String searchedFile;
+        int id;
+
+        public void setSerchedFile(String fName) {
+            searchedFile = fName;
+        }
+
+        public String findPath() throws IOException {
+            id = 0;
+
+            fileSystem = FileSystems.getDefault();
+            Files.walkFileTree(fileSystem.getPath("\\Users\\RAZER\\workspace"),
+                    new SimpleFileVisitor<java.nio.file.Path>() {
+                        @Override
+                        public FileVisitResult visitFile(java.nio.file.Path path, BasicFileAttributes attrs) {
+                            String tmp = String.valueOf(path.getFileName());
+                            if (tmp.equals(searchedFile)) {
+                                System.out.println(" path = " + path);
+                                Path resalt = path;
+                                pathF += resalt;
+                            }
+                            return FileVisitResult.CONTINUE;
+                        }
+                    });
+            return pathF;
+        }
+    }
+    @Override
+    public String findFile(String path){
+        String result;
+
+        FileService fileService = new FileService();
+        fileService.setSerchedFile(path);
+        try {
+            result = fileService.findPath();
+        }catch (IOException io){
+            return "404";
+        }
+
+        return result;
+    }
 }
---
+++ roman_rudych/src/main/java/service/FileFindServiceInteface.java
@@ -0,0 +1,9 @@
+package service;
+
+/**
+ * Created by RAZER on 17.08.2015.
+ */
+public interface FileFindServiceInteface {
+    String findFile(String path);
+
+}
