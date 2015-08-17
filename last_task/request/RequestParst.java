ab50c7163589b33d3bb6de57c10720c1fbfeb70f
GeorgeRubliov <gosha_rublev@ukr.net>
2015-08-17 20:35:07 +0300

    ftp Team

--- last_task/request/RequestParst.java
+++ last_task/request/RequestParst.java
@@ -1,6 +1,8 @@
 /**
  * Created by george on 17.08.15.
  */
-public interface RequestParst {
-    public String parsRequest(String request) throws BadRequestException, TooLongHeaderException;
+public class RequestParst {
+
+
+
 }
--- last_task/request/requestParst.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by george on 17.08.15.
- */
-public class requestParst {
-
-
-
-}
